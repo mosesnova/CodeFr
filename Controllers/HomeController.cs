8cd06c0a0a67b25cb5aab7ba283815361a6b5dbc
Moses Nova <moses@npl.nz>
2023-02-11 10:21:51 +1300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using CodeFr.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace CodeFr.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private CompanyContext context;
+
+        public HomeController(ILogger<HomeController> logger, CompanyContext cc)
+        {
+             context = cc;
+        }
+
+        public IActionResult Index()
+        {
+            var info = new Information()
+            {
+                Name = "YogiHosting",
+                License = "XXYY",
+                Revenue = 1000,
+                Establshied = Convert.ToDateTime("2014/06/24")
+            };
+            context.Entry(info).State = EntityState.Added;
+            context.SaveChanges();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult CreateInformation()
+        {
+            var info = new Information()
+            {
+                Name = "YogiHosting",
+                License = "XXYY",
+                Revenue = 1000,
+                Establshied = Convert.ToDateTime("2014/06/24")
+            };
+            context.Entry(info).State = EntityState.Added;
+            context.SaveChanges();
+
+            return View();
+        }
+    }
+}
---
+++ Models/CompanyContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace CodeFr.Models
+{
+    public class CompanyContext : DbContext
+    {
+        public CompanyContext(DbContextOptions<CompanyContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Information> Information { get; set; }
+    }
+}
